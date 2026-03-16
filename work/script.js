ff03f12aa5b52300c63ed45cfc1ead8fd77c36a8
alex-kasapov <askasapov@gmail.com>
2026-03-16 20:16:32 +0300

    16.03.2026

---
+++ work/script.js
@@ -0,0 +1,13 @@
+let daysOfWeek = [
+  "Понедельник",
+  "Вторник", 
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+  "Воскресенье"
+];
+
+for (let i = 0; i < daysOfWeek.length; i++) {
+  console.log(daysOfWeek[i]);
+}
